4be7e81e201bc2886115e0c1de7d228066de18d9
khermano <khermano@redhat.com>
2019-07-09 12:17:59 +0200

    ref maven test

--- src/test/java/org.xstefank.lt/SomeTest.java
+++ src/test/java/org.xstefank.lt/SomeTest.java
@@ -11,6 +11,6 @@ public class SomeTest {
 
     @Test
     public void mavenTest() {
-        Assert.assertEquals(System.getProperty("org.apache.maven.plugins.maven-compiler-plugin.version"), "3.0.5");
+        Assert.assertEquals(getClass().getPackage().getImplementationVersion(), "3.0.5");
     }
 }
